f3331dc4ac498b21a51fbb9574bece75f9d1e64f
jamestodd313 <jamestodd313@gmail.com>
2024-06-09 10:38:10 -0500

    interface: [Button] styling

--- components/elements/Button.tsx
+++ components/elements/Button.tsx
@@ -1,14 +1,31 @@
-import { Pressable, Text } from 'react-native'
+import { Pressable, StyleSheet, Text } from 'react-native'
 import React from 'react'
+import { blue, white } from '@/styles/colors'
 
+// STYLES
+const buttonStyles = StyleSheet.create({
+  universal: {
+    padding: 16,
+    backgroundColor: blue,
+    borderRadius: 8,
+  },
+  default: {
+    opacity: 1
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+})
+
+// TS INTERFACE
 interface propTypes {
   text: string,
   func: Function
 }
 const Button: React.FC<propTypes> = ({text, func}) => {
   return (
-    <Pressable>
-      <Text>{text}</Text>
+    <Pressable hitSlop={10} onPress={()=>func} style={({ pressed }) => [buttonStyles.universal, pressed ? buttonStyles.pressed : buttonStyles.default]}>
+      <Text style={{color: white}}>{text}</Text>
     </Pressable>
   )
 }
