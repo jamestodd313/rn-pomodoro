7b4f5f06c60564c6d38f94dc53f40b2422dae44e
jamestodd313 <jamestodd313@gmail.com>
2024-06-10 19:12:48 -0500

    interface: [Button] changed 'func' prop type to GestureResponderEvent

--- components/elements/Button.tsx
+++ components/elements/Button.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text } from 'react-native'
+import { GestureResponderEvent, Pressable, StyleSheet, Text } from 'react-native'
 import React from 'react'
 import { blue, white } from '@/styles/colors'
 
@@ -15,17 +15,22 @@ const buttonStyles = StyleSheet.create({
   pressed: {
     opacity: 0.7,
   },
+  text: {
+    fontWeight: "600",
+    color: white,
+    textAlign: "center"
+  }
 })
 
 // TS INTERFACE
 interface propTypes {
   text: string,
-  func: Function
+  func: (event: GestureResponderEvent)=> void;
 }
 const Button: React.FC<propTypes> = ({text, func}) => {
   return (
-    <Pressable hitSlop={10} onPress={()=>func} style={({ pressed }) => [buttonStyles.universal, pressed ? buttonStyles.pressed : buttonStyles.default]}>
-      <Text style={{color: white, textAlign: "center"}}>{text}</Text>
+    <Pressable hitSlop={10} onPress={func} style={({ pressed }) => [buttonStyles.universal, pressed ? buttonStyles.pressed : buttonStyles.default]}>
+      <Text style={buttonStyles.text}>{text}</Text>
     </Pressable>
   )
 }
