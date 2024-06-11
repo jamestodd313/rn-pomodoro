84d30fa8f583b89b0d10ef548aaf29d8fce0610b
jamestodd313 <jamestodd313@gmail.com>
2024-06-10 19:27:49 -0500

    logic: [Timer] setup timer context

--- app/(tabs)/timer.tsx
+++ app/(tabs)/timer.tsx
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { createContext } from 'react'
 import { layoutStyles } from '@/styles/layout'
 import Heading1 from '@/components/headings/Heading1'
 import Timer from '@/components/features/Timer'
 import Stats from '@/components/features/Stats'
 
+// Context
+const TimerContext = createContext({
+  timeRemaining: 0,
+  isRunning: false,
+  timerType: "focus", // timer types: focus, short break, long break
+  focusCompleted: 0,
+  breaksCompleted: 0,
+  startTimer: ()=> console.log("start"),
+  pauseTimer: ()=> console.log("pause"),
+  skipTimer: ()=> console.log("skip")
+})
+
+// Component
 const timer = () => {
   return (
     <View style={layoutStyles.main}>
