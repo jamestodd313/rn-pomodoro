70ca26f01c09ce5efb340c2325fb789a9c37b436
jamestodd313 <jamestodd313@gmail.com>
2024-06-11 19:48:16 -0500

    logic: [Timer] got TimerContext working and setup for use

--- app/(tabs)/timer.tsx
+++ app/(tabs)/timer.tsx
@@ -5,19 +5,9 @@ import Heading1 from '@/components/headings/Heading1'
 import Timer from '@/components/features/Timer'
 import Stats from '@/components/features/Stats'
 
-// Context
-const TimerContext = createContext({
-  timeRemaining: 0,
-  isRunning: false,
-  timerType: "focus", // timer types: focus, short break, long break
-  focusCompleted: 0,
-  breaksCompleted: 0,
-  startTimer: ()=> console.log("start"),
-  pauseTimer: ()=> console.log("pause"),
-  skipTimer: ()=> console.log("skip")
-})
 
-// Component
+
+// Page
 const timer = () => {
   return (
     <View style={layoutStyles.main}>
--- components/features/Timer.tsx
+++ components/features/Timer.tsx
@@ -1,5 +1,18 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {createContext, useContext, useState} from 'react'
+import Button from '../elements/Button'
+
+// Context
+const TimerContext = createContext({
+  timeRemaining: 0,
+  isRunning: false,
+  timerType: "focus", // timer types: focus, short break, long break
+  focusCompleted: 0,
+  breaksCompleted: 0,
+  startTimer: ()=> console.log("start"),
+  pauseTimer: ()=> console.log("pause"),
+  skipTimer: ()=> console.log("skip")
+})
 
 // TS Interface
 interface propTypes {
@@ -8,14 +21,39 @@ interface propTypes {
 
 // Component
 const Timer: React.FC<propTypes> = ({location}) => {
+  // State
+  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [timerType, setTimerType] = useState("focus");
+  const [focusCompleted, setFocusCompleted] = useState(0);
+  const [breaksCompleted, setBreaksCompleted] = useState(0);
+  // Controls
+  const startTimer = ()=> {
+    console.log("start timer");
+  }
+  const pauseTimer = ()=> {
+    console.log("pause timer");
+  }
+  const skipTimer = ()=> {
+    console.log("skip timer");
+  }
   return (
-    <View>
-      {location === "dashboard" ? (
-        <Text>Dashboard Timer</Text>
-      ) : (
-        <Text>Timer Page Timer</Text>
-      )}
-    </View>
+    <TimerContext.Provider value={{timeRemaining, isRunning, timerType, focusCompleted, breaksCompleted, startTimer, pauseTimer, skipTimer}}>
+      <View>
+        {location === "dashboard" ? (
+          // Dashboard Page Timer Component
+          <View>
+            <Text>Dashboard Timer</Text> 
+          </View>
+        ) : (
+          // Timer Page Timer Component
+          <View>
+            <Text>Timer Page Timer</Text>
+            <Button text={isRunning ? "Pause" : "Start"} func={startTimer}/>
+          </View>
+        )}
+      </View>
+    </TimerContext.Provider>
   )
 }
 
