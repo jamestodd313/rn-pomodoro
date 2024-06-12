0c3ba710c96d90eb7be22e0819bb56967287f0de
jamestodd313 <jamestodd313@gmail.com>
2024-06-12 15:26:58 -0500

    logic: [Context] refactored context, created ContextProvider component, wrapped (tabs) _layout, implemented useContext in Timer component

--- components/features/Timer.tsx
+++ components/features/Timer.tsx
@@ -1,18 +1,8 @@
 import { View, Text } from 'react-native'
-import React, {createContext, useContext, useState} from 'react'
+import React, { useContext } from 'react'
 import Button from '../elements/Button'
+import { TimerContext } from '@/context/ContextProvider'
 
-// Context
-const TimerContext = createContext({
-  timeRemaining: 0,
-  isRunning: false,
-  timerType: "focus", // timer types: focus, short break, long break
-  focusCompleted: 0,
-  breaksCompleted: 0,
-  startTimer: ()=> console.log("start"),
-  pauseTimer: ()=> console.log("pause"),
-  skipTimer: ()=> console.log("skip")
-})
 
 // TS Interface
 interface propTypes {
@@ -21,24 +11,9 @@ interface propTypes {
 
 // Component
 const Timer: React.FC<propTypes> = ({location}) => {
-  // State
-  const [timeRemaining, setTimeRemaining] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [timerType, setTimerType] = useState("focus");
-  const [focusCompleted, setFocusCompleted] = useState(0);
-  const [breaksCompleted, setBreaksCompleted] = useState(0);
-  // Controls
-  const startTimer = ()=> {
-    console.log("start timer");
-  }
-  const pauseTimer = ()=> {
-    console.log("pause timer");
-  }
-  const skipTimer = ()=> {
-    console.log("skip timer");
-  }
+  let {timerType, timeRemaining, isRunning, focusCompleted, breaksCompleted} = useContext(TimerContext)
+
   return (
-    <TimerContext.Provider value={{timeRemaining, isRunning, timerType, focusCompleted, breaksCompleted, startTimer, pauseTimer, skipTimer}}>
       <View>
         {location === "dashboard" ? (
           // Dashboard Page Timer Component
@@ -49,11 +24,10 @@ const Timer: React.FC<propTypes> = ({location}) => {
           // Timer Page Timer Component
           <View>
             <Text>Timer Page Timer</Text>
-            <Button text={isRunning ? "Pause" : "Start"} func={startTimer}/>
+            {/* <Button text={isRunning ? "Pause" : "Start"} func={!isRunning ? startTimer : pauseTimer}/> */}
           </View>
         )}
       </View>
-    </TimerContext.Provider>
   )
 }
 
